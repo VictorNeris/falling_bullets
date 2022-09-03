fe719eefb02f9a574e8870b45bf3b9fe45f182fc
VictorNeris <38917015+VictorNeris@users.noreply.github.com>
2022-09-03 15:42:18 -0300

    Zoom dymanics, and stuff

--- _patch/_patch.bat
+++ _patch/_patch.bat
@@ -1,4 +1,4 @@
 7-Zip\7z.exe a -tzip ..\Common_server.zip .\Common_server\** -r
 7-Zip\7z.exe a -tzip ..\Objects_server.zip .\Objects_server\** -r
 7-Zip\7z.exe a -tzip ..\objects_weapons_server.zip .\objects_weapons_server\** -r
-
+7-Zip\7z.exe a -tzip ..\Menu_server.zip .\Menu_server\** -r
