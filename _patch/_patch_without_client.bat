564cf5ae139c9cb998decef3651628f6c51ce895
VictorNeris <38917015+VictorNeris@users.noreply.github.com>
2022-10-17 19:52:56 -0300

    Fixed SV-98 PSO07

--- _patch/_patch_without_client.bat
+++ _patch/_patch_without_client.bat
@@ -1,5 +1,4 @@
 7-Zip\7z.exe a -tzip ..\Common_server.zip .\Common_server\** -r
 7-Zip\7z.exe a -tzip ..\Objects_server.zip .\Objects_server\** -r
 7-Zip\7z.exe a -tzip ..\objects_weapons_server.zip .\objects_weapons_server\** -r
-7-Zip\7z.exe a -tzip ..\objects_weapons_client.zip .\objects_weapons_client\** -r
 7-Zip\7z.exe a -tzip ..\Menu_server.zip .\Menu_server\** -r
